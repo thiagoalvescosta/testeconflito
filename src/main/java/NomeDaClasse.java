1b51fb0ef3be4dc26cb59449a800d461a8029dde
Usuário de Teste <thiagoalvescosta@gmail.com>
2018-04-02 08:55:25 -0300

    First commit...

---
+++ src/main/java/NomeDaClasse.java
@@ -0,0 +1,23 @@
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Usuário de Teste
+ * @version 1.0
+ * @since 2018-03-28
+ *
+ */
+ 
+public class NomeDaClasse {
+
+
+	/**
+	 * Construtor
+	 **/
+	public NomeDaClasse (){
+	  
+	  asd
+	}
+
+}
